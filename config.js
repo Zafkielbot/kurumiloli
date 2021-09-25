e003715eae8dd27d8968a3f4f4c0dda43e0934c3
Zafkielbot <83331706+Zafkielbot@users.noreply.github.com>
2021-09-25 23:37:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.author = 'Rafli'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '*Server Error*'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&backgroundColor=%23101820&fillTextPattern=Warning!&text='
 global.watermark = '® Kurumiloli'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
