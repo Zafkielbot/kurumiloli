feda0f91ecfde527fbadd7a7e0f716a09a003b90
Zafkielbot <83331706+Zafkielbot@users.noreply.github.com>
2021-10-13 06:06:47 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,8 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
 ├ Pulsa:-08998512588
-├ Dana:-08998512588
 ├ OVO:-08998512588
+├ Dana:-08998512588
+├Transfer uangnya pakai DANA aja. 
+├Lebih praktis, transfer di sini ya
+├ https://link.dana.id/qr/fkno783
 └────
 `.trim(), watermark, 'Donasi', '.donasi', m)
 handler.help = ['donasi']
