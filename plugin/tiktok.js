eea17a86941a57a6e4b27c6f4d0ecb24a0e3db31
Najwanzaki <92720345+Najwanzaki@users.noreply.github.com>
2021-10-19 10:28:59 +0700

    Update and rename plugins/tiktok.js to plugins/.tiktok.js

--- plugin/tiktok.js
+++ plugin/tiktok.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let json = await res.json()
   // if (!json.status) throw json        
   await m.reply(wait)
-  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n® Kurumiloli`, m)
+  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\n® Zaky-BOTZ`, m)
 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
